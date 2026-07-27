611b85e672264abf0cd8dae3b29d115f2e5970e9
Sumathi_A <sumathi.a1606@gmail.com>
2026-07-27 21:05:23 +0530

    LeetCode Problem - 349. Intersection of two arrays

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -9,12 +9,12 @@ public class IntersectionOfTwoArrays {
         int[] arr1 = {1,2,4,3,2,5};
         int[] arr2 = {2,3,5,6,7};
         System.out.print("Intersection of the two arrays: ");
-
+//1st loop - Search for elements of arr1 and add them to set1
         for (int i=0 ; i<arr1.length; i++)
     {
         set1.add(arr1[i]);
     }
-
+//2nd loop = Save the elements of arr2 that are present in set1 to set2
         for (int j=0; j<arr2.length;j++)
         {
             if(set1.contains(arr2[j]))
